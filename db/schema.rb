bad45315672a9b1b5cca1b06f8be4184d8d143c3
Sanjay <sanjay.yadav1@rsystems.com>
2019-05-31 09:05:33 +0530

    migration completed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_122737) do
+ActiveRecord::Schema.define(version: 2019_05_30_135705) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -59,6 +59,18 @@ ActiveRecord::Schema.define(version: 2019_05_30_122737) do
     t.index ["admin_user_id"], name: "index_categories_on_admin_user_id"
   end
 
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "category_id"
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 10
+    t.boolean "status"
+    t.string "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "salutaion"
     t.string "first_name"
@@ -80,4 +92,5 @@ ActiveRecord::Schema.define(version: 2019_05_30_122737) do
   end
 
   add_foreign_key "categories", "admin_users"
+  add_foreign_key "products", "categories"
 end
