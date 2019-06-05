844b3705f22e7710c9a3e91e283bf6b1e8d299b4
Sanjay <sanjay.yadav1@rsystems.com>
2019-06-05 10:50:46 +0530

    Made association of cattle and varient.

--- app/models/cattle.rb
+++ app/models/cattle.rb
@@ -1,6 +1,4 @@
 class Cattle < ApplicationRecord
   belongs_to :admin_user
-  belongs_to :cattle_category
-  belongs_to :cattle_breed
   belongs_to :cattle_variant
 end
--- app/models/cattle_variant.rb
+++ app/models/cattle_variant.rb
@@ -1,3 +1,4 @@
 class CattleVariant < ApplicationRecord
   belongs_to :cattle_breed
+  has_many :cattles
 end
