d279dd614b54d3b38b32ca0d9b01f266793ea183
Sanjay <sanjay.yadav1@rsystems.com>
2019-06-06 10:53:39 +0530

    Commented references in price table.

--- db/migrate/20190605190556_create_prices.rb
+++ db/migrate/20190605190556_create_prices.rb
@@ -1,8 +1,8 @@
 class CreatePrices < ActiveRecord::Migration[5.2]
   def change
     create_table :prices do |t|
-      t.references :product, foreign_key: true
-      t.references :servise_state, foreign_key: true
+      # t.references :product, foreign_key: true
+      # t.references :servise_state, foreign_key: true
       t.string :title, limit: 110
       t.decimal :price, precision: 7, scale: 2
       t.text :description
